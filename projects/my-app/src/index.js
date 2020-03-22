c961eddd802142b75f23984be604abc17347736a
Mike <Wong>
2020-03-22 13:25:43 +0800

    added root reducer and redux packages

--- projects/my-app/src/index.js
+++ projects/my-app/src/index.js
@@ -5,14 +5,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './custom.scss';
+import { createStore } from 'redux';
+import { provider, Provider } from 'react-redux';
 
 // import App from './App';
 import PokeApp from './PokeApp';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './reduces/rootReducer';
+const store = createStore(rootReducer);
 
-ReactDOM.render(<PokeApp />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    {' '}
+    <PokeApp />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
